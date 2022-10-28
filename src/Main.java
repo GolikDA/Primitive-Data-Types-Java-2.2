4f077cfa8a8f85192f0c61515ff46da08b5968a8
Dima Golik <1dimagolik1@mail.ru>
2022-10-28 21:11:43 +0700

    main changes
    Отредактировано название переменной в 5 строке, измненена логика расчета бонуса в 5 и 7 строке.
    сокращены строки вывода на экран.

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,20 @@ public class Main {
     public static void main(String[] args) {
         int balance = 100;
         int payment = 1100;
-        int x = 100 * 1;
+        int rubleToBonusRatio = 100 / 1;
 
-        int bonus = payment >= 1000 ? payment / x : 0;
+        int bonus = payment > 1000 ? payment / rubleToBonusRatio : 0;
 
         /*
         int bonus;
-        if (payment >= 1000) {
+        if (payment > 1000) {
             bonus = payment / x;
         } else {
             bonus = 0;
         }
         */
 
-        System.out.println("Баланс:");
-        System.out.println(balance + bonus + payment);
-
-        System.out.println("Бонус:");
-        System.out.println(bonus);
+        System.out.println("Баланс: " + (balance + bonus + payment));
+        System.out.println("Бонус: " + bonus);
     }
 }
